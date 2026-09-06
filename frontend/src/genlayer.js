70479387dfbdaec8d39b6c67977f40ee73bd5242
Naseer32 <76082276+Naseer32@users.noreply.github.com>
2026-09-06 17:12:56 +0100

    Update CONTRACT_ADDRESS to Testnet Bradbury address

--- frontend/src/genlayer.js
+++ frontend/src/genlayer.js
@@ -2,7 +2,7 @@ import { createClient } from "genlayer-js";
 import { testnetBradbury } from "genlayer-js/chains";
 
 // TODO: replace with the deployed Arbiter contract address on Testnet Bradbury
-export const CONTRACT_ADDRESS = "0x0000000000000000000000000000000000000000";
+export const CONTRACT_ADDRESS = "0xEF16CB5F1b8958e83dcaaaADCee20342Ce56ba09";
 
 // Add Bradbury to the connected wallet if it isn't already configured there.
 // MetaMask (and most injected wallets) support wallet_addEthereumChain.
